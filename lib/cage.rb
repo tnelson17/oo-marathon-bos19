dc18818949ee64a0dc526efe6a8aa7e7a13f8e0a
Travis Nelson <travisnelson@Traviss-MacBook-Pro.local>
2017-12-07 12:05:20 -0500

    initial commit

---
+++ lib/cage.rb
@@ -0,0 +1,17 @@
+class Cage
+attr_accessor :animal
+attr_reader :cage
+
+  # def initialize
+  #
+  #
+  # end
+  #
+  # def animal
+  # end
+
+  def empty?
+    cage.nil?
+  end
+
+end
---
+++ lib/python.rb
@@ -0,0 +1,14 @@
+class Python
+  include Reptile
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+
+  def speak
+    "SSSSssssss..."
+  end
+
+
+
+end
